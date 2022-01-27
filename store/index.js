c0cd41e4718c77d670963ba8fafb4bdb6fade8b9
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-26 20:51:00 -0600

    add

---
+++ store/index.js
@@ -0,0 +1,17 @@
+export const state = () => ({
+    tasks: []
+})
+
+export const mutations = {
+    ADD_TASK(state, task) {
+        state.tasks = [{ content: task, done: false }, ...state.tasks];
+        // state.tasks.push({ content: task, done: false})
+        //state.task.unshift({ content: task, done: false})
+    },
+    REMOVE_TASK(state, task) {
+        state.tasks.splice(state.tasks.indexOf(task), 1);
+    },
+    TOGGLE_TASK(state, task) {
+        task.done = !task.done;
+    }
+}
